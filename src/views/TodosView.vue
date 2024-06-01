2afd43af8f5d41b54355b0b6856601ddaea0dc9c
srimanasay <88306084+srimanasay@users.noreply.github.com>
2024-06-01 12:20:27 +0530

    Add files via upload

---
+++ src/views/TodosView.vue
@@ -0,0 +1,112 @@
+<script setup>
+import TodoCreater from "../components/TodoCreater.vue";
+import TodoItem from "../components/TodoItem.vue";
+import { Icon } from '@iconify/vue';
+import { ref, watch, computed } from "vue";
+import { uid } from 'uid';
+const todoList = ref([]);
+
+watch(todoList, () => {
+    setTodoListLocalStorage();
+}, {
+    deep: true,
+});
+
+const todocompleted = computed(() => {
+    return todoList.value.every((todo) => todo.isCompleted);
+});
+
+const fetchTodoList = () => {
+    const savedTodoList = JSON.parse(localStorage.getItem("todoList"));
+    if (savedTodoList) {
+        todoList.value = savedTodoList;
+    }
+};
+
+fetchTodoList();
+
+const setTodoListLocalStorage = () => {
+    localStorage.setItem("todoList", JSON.stringify(todoList.value));
+};
+
+const createTodo = (todo) => {
+    todoList.value.push(
+        {
+            id: uid(),
+            todo,
+            isCompleted: null,
+            isEditing: null,
+        }
+    );
+    setTodoListLocalStorage();
+};
+
+const toogleTodoComplete = (todoPos) => {
+    todoList.value[todoPos].isCompleted = !todoList.value[todoPos].isCompleted;
+};
+
+const toogleEditTodo = (todoPos) => {
+    todoList.value[todoPos].isEditing = !todoList.value[todoPos].isEditing;
+};
+
+const updateTodo = (todoVal, todoPos) => {
+    todoList.value[todoPos].todo = todoVal;
+};
+const deleteTodo = (todoId) => {
+    todoList.value = todoList.value.filter((todo) => todo.id !== todoId);
+};
+
+</script>
+
+<template>
+    <main>
+        <h1>Create Todo</h1>
+        <TodoCreater @create-todo="createTodo" />
+        <ul class="todo-list" v-if="todoList.length > 0">
+            <TodoItem v-for="(todo, index) in todoList" :todo="todo" :index="index"
+                @toogle-complete="toogleTodoComplete" @edit-todo="toogleEditTodo" @update-todo="updateTodo"
+                @delete-todo="deleteTodo" />
+        </ul>
+        <p class="todos-msg" v-else>
+            <Icon icon="noto-v1:sad-but-relieved-face" width="22" />
+            <span>You have no todo's to complete! Add one!</span>
+        </p>
+        <p v-if="todocompleted && todoList.length > 0" class="todos-msg">
+            <Icon icon="noto-v1:party-popper" />
+            <span>Congratulations! You have completed all your
+                todos!</span>
+        </p>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+    display: flex;
+    flex-direction: column;
+    max-width: 500px;
+    width: 100%;
+    margin: 0 auto;
+    padding: 40px 16px;
+}
+
+h1 {
+    margin-bottom: 16px;
+    text-align: center;
+}
+
+.todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+}
+
+.todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
+}
+</style>
